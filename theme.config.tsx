b24a0abe6ae916c38145e1eb0b407a26c612159f
oslook <oslook@users.noreply.github.com>
2024-03-01 15:19:33 +0000

    add side

--- theme.config.tsx
+++ theme.config.tsx
@@ -37,7 +37,63 @@ const config: DocsThemeConfig = {
         titleTemplate: '%s – PingoraRust.COM'
       }
     }
-  }
+  },
+
+  head: () => {
+    const { asPath, defaultLocale, locale } = useRouter()
+    const { frontMatter } = useConfig()
+    const url =
+      'https://www.pingorarust.com' +
+      (defaultLocale === locale ? asPath : `/${locale}${asPath}`)
+
+    return (
+      <>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta
+          property="description"
+          content={frontMatter.description || 'Pingora Rust'}
+        />
+        <meta property="og:url" content={url} />
+        <meta property="og:title" content={frontMatter.title || 'pingorarust.com'} />
+        <meta property="og:image" content={frontMatter.title || 'pingorarust.com'} />
+        <meta property="twitter:title" content={frontMatter.title || 'pingorarust.com'}></meta>
+        <meta property="twitter:image" content="Twitter link preview image URL"></meta>
+        <meta property="twitter:card" content="summary_large_image"></meta>
+        <meta
+          property="twitter:description"
+          content={frontMatter.description || '....'}
+        />
+        <meta
+          property="og:description"
+          content={frontMatter.description || '......'}
+        />
+      </>
+    )
+  },
+
+  sidebar: {
+    titleComponent({ title, type }) {
+      if (type === 'separator') {
+        return (
+          <div style={{ background: 'cyan', textAlign: 'center' }}>{title}</div>
+        )
+      }
+      if (title === 'About') {
+        return <>❓ {title}</>
+      }
+      return <>👉 {title}</>
+    }
+  },
+  feedback: {
+    content: null
+  },
+  navigation: {
+    prev: true,
+    next: true
+  },
+  editLink: {
+    text: null
+  },
   // ... other theme options
 }
 
